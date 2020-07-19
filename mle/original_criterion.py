c424408e7f587c539e5136c50152c59f9f785372
ozakiryota <kb22056@gmail.com>
2020-07-20 03:54:30 +0900

    updated

--- mle/original_criterion.py
+++ mle/original_criterion.py
@@ -5,18 +5,15 @@ import torch
 
 def getCov(x):
     sigma = x[:, 3:6].clone()
-    print("sigma[:, 0] = ", sigma[:, 0])
     corr = x[:, 6:9].clone()
-    print("corr[0] = ", corr[0])
     sxx = sigma[:, 0] * sigma[:, 0]
-    print("sxx = ", sxx)
     syy = sigma[:, 1] * sigma[:, 1]
     szz = sigma[:, 2] * sigma[:, 2]
     sxy = corr[:, 0] * sigma[:, 0] * sigma[:, 1]
     syz = corr[:, 1] * sigma[:, 1] * sigma[:, 2]
     szx = corr[:, 2] * sigma[:, 2] * sigma[:, 0]
 
-    cov = torch.empty(sigma.size(0), 3, 3)
+    cov = torch.empty(sigma.size(0), sigma.size(1), sigma.size(1))
     cov[:, 0, 0] = sxx
     cov[:, 0, 1] = sxy
     cov[:, 0, 2] = szx
@@ -35,58 +32,42 @@ def computeDet(m):
 def originalCriterion(outputs, labels):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     k = labels.size(1)
-    g = outputs[:, :3].clone()
+    mu = outputs[:, :3].clone()
     cov = getCov(outputs)
-    cov_inv = torch.inverse(cov)
-    cov_det = computeDet(cov)
-    diff = labels - g
-    diff = diff.unsqueeze_(1)
-    diff_trans = torch.transpose(diff, 1, 2)
-    # print("k = ", k)
-    # print("cov.size() = ", cov.size())
-    # print("cov = ", cov)
-    # print("cov_inv.size() = ", cov_inv.size())
-    # print("cov_inv = ", cov_inv)
-    # print("diff.size() = ", diff.size())
-    # print("diff = ", diff)
-    # print("diff_trans.size() = ", diff_trans.size())
-    # print("diff_trans = ", diff_trans)
-    print("cov[0] = ", cov[0])
-    print("cov_inv[0] = ", cov_inv[0])
 
-    cov_inv = cov_inv.to(device)
-    numerator = torch.exp(-1 / 2 * torch.bmm(torch.bmm(diff, cov_inv), diff_trans))
-    numerator = numerator.clone().squeeze_()
-    # denominator = torch.sqrt(((2*math.pi)**k) * torch.det(cov)) #det() works in torch>1.2.0
-    denominator = torch.sqrt(((2*math.pi)**k) * cov_det)
-    denominator = denominator.to(device)
-    # print("denominator.size() = ", denominator.size())
-    # print("denominator = ", denominator)
-    # print("numerator.size() = ", numerator.size())
-    # print("numerator = ", numerator)
-    loss = numerator / denominator
-    epsiron = 1e-20
-    loss = -torch.log(torch.clamp(loss, min=epsiron))
-    loss = torch.sum(loss) / loss.size(0)
+    # cov_inv = torch.inverse(cov)
+    # cov_det = computeDet(cov)
+    # diff = labels - mu
+    # diff = diff.unsqueeze_(1)
+    # diff_trans = torch.transpose(diff, 1, 2)
+    #
+    # cov_inv = cov_inv.to(device)
+    # numerator = torch.exp(-1 / 2 * torch.bmm(torch.bmm(diff, cov_inv), diff_trans))
+    # numerator = numerator.clone().squeeze_()
+    # # denominator = torch.sqrt(((2*math.pi)**k) * torch.det(cov)) #det() works in torch>1.2.0
+    # denominator = torch.sqrt(((2*math.pi)**k) * cov_det)
+    # denominator = denominator.to(device)
+    # loss = numerator / denominator
+    # epsiron = 1e-20
+    # loss = -torch.log(torch.clamp(loss, min=epsiron))
+    # print("loss = ", loss)
+    # loss = torch.sum(loss) / loss.size(0)
+    # print("loss = ", loss)
 
-    for i in range(cov.size(0)):
-        if torch.isnan(denominator[i]).any():
-            print("!!!!!!!!!!!!!!!!!!!!!!!")
-            print("torch.isnan(denominator[i]).any() = ", torch.isnan(denominator[i]).any())
+    cov = cov.to(device)
+    print("cov = ", cov)
+    m = torch.distributions.MultivariateNormal(mu, cov)
+    loss = -m.log_prob(labels)
+    print("loss = ", loss)
+    for i in range(loss.size(0)):
+        if torch.isnan(loss[i]):
+            print("torch.isnan(loss[i]) = ", torch.isnan(loss[i]))
             print("outputs[i] = ", outputs[i])
             print("cov[i] = ", cov[i])
-            print("cov_det[i] = ", cov_det[i])
-    print("torch.isnan(outputs).any() = ", torch.isnan(outputs).any())
-    print("torch.isnan(cov).any() = ", torch.isnan(cov).any())
-    print("cov_det = ", cov_det)
-    print("((2*math.pi)**k) * cov_det = ", ((2*math.pi)**k) * cov_det)
-    print("denominator = ", denominator)
-    print("torch.isnan(((2*math.pi)**k) * cov_det).any() = ", torch.isnan(((2*math.pi)**k) * cov_det).any())
-    print("torch.isnan(denominator).any() = ", torch.isnan(denominator).any())
-    print("torch.isnan(loss).any() = ", torch.isnan(loss).any())
+    loss = loss.mean()
     print("loss = ", loss)
 
-    # loss = torch.mean((g - labels)**2)
+    # loss = torch.mean((mu - labels)**2)
     return loss
 
 ##### test #####
