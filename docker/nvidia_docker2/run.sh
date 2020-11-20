257710d59035e2e0a51e535e3c0c77a01e601a1a
ozakiryota <kb22056@gmail.com>
2020-11-20 13:56:43 +0900

    updated

--- docker/nvidia_docker2/run.sh
+++ docker/nvidia_docker2/run.sh
@@ -11,7 +11,7 @@ docker run -it --rm \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 	-v $root_path/../../../dataset_image_to_gravity:/home/$image_name/../dataset_image_to_gravity \
-	-v $root_path/../weights:/home/$image_name/weights \
-	-v $root_path/../graph:/home/$image_name/graph \
+	-v $root_path/../../weights:/home/$image_name/weights \
+	-v $root_path/../../graph:/home/$image_name/graph \
 	-v $root_path/../../logs:/home/$image_name/logs \
 	$image_name:$tag_name
