3bb46fc491bb62d5453e7e0303f68580de84c372
ozakiryota <37431972+ozakiryota@users.noreply.github.com>
2021-08-15 17:02:56 +0900

    Update compute_ave_std_of_dataset.py

--- pysrc/statistics/compute_ave_std_of_dataset.py
+++ pysrc/statistics/compute_ave_std_of_dataset.py
@@ -11,6 +11,7 @@ class StatisticsModel:
         ## list
         self.list_data = make_datalist_mod.makeDataList(list_rootpath, csv_name)
         self.list_error_rp = []
+        self.list_error_g_angle = []
 
     def __call__(self):
         self.computeAttitudeError()
@@ -24,9 +25,13 @@ class StatisticsModel:
         ## error
         for acc in list_acc:
             acc = [float(num) for num in acc]
+            ## error in roll and pitch
             label_rp = self.accToRP(acc)
             error_rp = self.computeAngleDiff(ave_rp, label_rp)
             self.list_error_rp.append(error_rp)
+            ## error in angle of g
+            error_g_angle = self.getAngleBetweenVectors(acc, ave_acc)
+            self.list_error_g_angle.append(error_g_angle)
         ## print
         print("ave_rp [deg] = ", ave_rp/math.pi*180.0)
 
@@ -40,11 +45,18 @@ class StatisticsModel:
         diff = np.arctan2(np.sin(angle1 - angle2), np.cos(angle1 - angle2))
         return diff
 
+    def getAngleBetweenVectors(v1, v2):
+        return math.acos(np.dot(v1, v2)/np.linalg.norm(v1, ord=2)/np.linalg.norm(v2, ord=2))
+
     def printError(self):
-        mae = self.computeMAE(np.array(self.list_error_rp)/math.pi*180.0)
-        var = self.computeVar(np.array(self.list_error_rp)/math.pi*180.0)
-        print("mae [deg] = ", mae)
-        print("var [deg^2] = ", var)
+        mae_rp = self.computeMAE(np.array(self.list_error_rp)/math.pi*180.0)
+        var_rp = self.computeVar(np.array(self.list_error_rp)/math.pi*180.0)
+        print("mae_rp [deg] = ", mae_rp)
+        print("var_rp [deg^2] = ", var_rp)
+        mae_g_angle = self.computeMAE(np.array(self.list_error_g_angle)/math.pi*180.0)
+        var_g_angle = self.computeVar(np.array(self.list_error_g_angle)/math.pi*180.0)
+        print("mae_g_angle [deg] = ", mae_g_angle)
+        print("var_g_angle [deg^2] = ", var_g_angle)
 
     def computeMAE(self, x):
         return np.mean(np.abs(x), axis=0)
