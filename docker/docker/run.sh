64ac266cd860039611504e7cf8628e26b8abd6fc
YoshikiMaekawa2000 <yohiki.maekawa@icloud.com>
2023-10-16 05:02:42 +0000

    dockerfile

--- docker/docker/run.sh
+++ docker/docker/run.sh
@@ -10,7 +10,7 @@ docker run -it --rm \
 	--env="DISPLAY" \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-	-v $root_path/../../../dataset_image_to_gravity:/home/$image_name/../dataset_image_to_gravity \
+	-v $root_path/../../../share/private/28th/maekawa/dataset/dataset_image_to_gravity:/home/$image_name/../dataset_image_to_gravity \
 	-v $root_path/../../weights:/home/$image_name/weights \
 	-v $root_path/../../graph:/home/$image_name/graph \
 	-v $root_path/../../logs:/home/$image_name/logs \
